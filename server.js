b0ac6ca7f177db7d7c403d3f2e25607a2641e5b8
ctghadiri <ctghadiri@gmail.com>
2019-05-01 15:26:16 -0700

    change force to true for sequelize sync

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ require("./routes")(app);
 
 
 // Start the API server after syncing sequelize models
-db.sequelize.sync({ force: false }).then(function() {
+db.sequelize.sync({ force: true }).then(function() {
   app.listen(PORT, function() {
     console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
   });
